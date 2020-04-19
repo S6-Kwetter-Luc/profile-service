814f80f1c975e443b0f21c8dd9293268dec5131a
Luc Urlings <urlingsluc@gmail.com>
2020-04-19 13:59:45 +0200

    KL-44: init project and github workflow

---
+++ profile-service-test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace profile_service_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
